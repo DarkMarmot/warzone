cae4aa31c6292fff52271a91bcd1006dcf04ad43
darkmarmot <scott.southworth@gmail.com>
2019-08-28 10:34:23 -0500

    adding messages

--- lib/warzone/battle_server.ex
+++ lib/warzone/battle_server.ex
@@ -93,7 +93,7 @@ defmodule Warzone.BattleServer do
   end
 
   def handle_cast({:debug, _player_pid}, %Battle{} = battle) do
-    battle.messages
+    IO.inspect(battle.messages)
     {:noreply, battle}
   end
 
